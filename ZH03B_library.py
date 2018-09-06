42e1bfe3f7c13794a3275119663a35489fa59fde
davester <spider-pig@comcast.net>
2018-09-05 21:26:23 -0700

    Holy smokin' syntax. And logic.
    Make the default function ReadSample work.

--- ZH03B_library.py
+++ ZH03B_library.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 #
-#   v0.1  ZH03B_library.py
+#   v0.2  ZH03B_library.py
 #   9/5/2018  Dave Thompson
 #
 #   ZH03B Python3 Library
@@ -9,21 +9,19 @@
 #
 #
 #
-import time
-import serial
 import binascii
+import serial
+import time
 
 ser = serial.Serial(
- port='/dev/ttyUSB2',
+ port='/dev/ttyUSB1',
  baudrate = 9600,
  parity=serial.PARITY_NONE,
  stopbits=serial.STOPBITS_ONE,
  bytesize=serial.EIGHTBITS,
  timeout=1
 )
-ser.flushInput() #flush input buffer
-sampled = False
-pwr-flag = ON
+#  ser.flushInput() #flush input buffer
 
 def HexToByte( hexStr ):
     """
@@ -66,16 +64,16 @@ def SetStream():
 def QAgetsample():
     """
     Q&A mode requires a command to obtain a reading sample
-    Returns: int PM1, int PM2.5, int PM10not turn on the fan.
-    Fail immediately if we can
+    Returns: int PM1, int PM25, int PM10
+    Fail immediately if we cannot turn on the fan if we are dormant.
     """
 
-    if pwr-flag = "ERROR"
-       return
+    if pwr-flag == "ERROR":
+       return (pwr-flag)
 
-    if pwr-flag = "OFF":
+    if pwr-flag == "OFF":
        DormantMode( "ON" )
-       sleep(1)
+       time.sleep(1)
        ser.write( b'0xFF, 0x01, 0x86, 0x00, 0x00, 0x00, 0x00, 0x00, 0x79')
        sampled = False
        while sampled != True:
@@ -92,7 +90,7 @@ def QAgetsample():
               print (" PM2.5 level: ", PM25, end="")
               print (" PM10 level: ", PM10 )
               DormantMode( "OFF" )
-              return ( PM1, PM2.5, PM10 )
+              return ( PM1, PM25, PM10 )
          else:
           continue
     else:
@@ -112,78 +110,71 @@ def QAgetsample():
               print (" PM2.5 level: ", PM25, end="")
               print (" PM10 level: ", PM10 )
               sampled = false
-              return ( PM1, PM2.5, PM10 )
+              return ( PM1, PM25, PM10 )
 
-           else:
+         else:
              continue
 
-def DormantMode(pwr-state):
+def DormantMode(pwr_status):
     """
     Turn dormant mode on or off. Must be on to measure.
     """
 
-    if pwr-state == "ON":
+    if pwr_status == "ON":
        ser.write( b'0xFF, 0x01, 0xA7, 0x00, 0x00, 0x00, 0x00, 0x00, 0x56')
        sampled = False
        while sampled != "True":
          status = ser.read(2)
-          if status != b'':
-              status =  HexToByte( ((binascii.hexlify(status)).hex()) )
-             if status == "FFA7":
-                sampled = "True"
-                status = int(HexToByte( ((binascii.hexlify(ser.read(1))).hex()) ),16)
-             if status == "1"
-                pwr-flag = "ON"
-             if status == "0"
-                pwr-flag = "ERROR"
-                return
-       else:
-         continue
-
-
-    If pwr-state == "OFF"
+         if status != b'':
+           status =  HexToByte( ((binascii.hexlify(status)).hex()) )
+           if status == "FFA7":
+            sampled = "True"
+            status = int(HexToByte( ((binascii.hexlify(ser.read(1))).hex()) ),16)
+           if status == "1":
+            pwr_status = "ON"
+           if status == "0":
+            pwr_status = "ERROR"
+            return (pwr_status)
+         else:
+           continue
+    if pwr_status == "OFF":
        ser.write( b'0xFF, 0x01, 0xA7, 0x01, 0x00, 0x00, 0x00, 0x00, 0x57')
        sampled = False
        while sampled != "True":
          status = ser.read(2)
-           if status != b'':
-             status =  HexToByte( ((binascii.hexlify(status)).hex()) )
-             if status == "FFA7":
-                sampled = "True"
-               status = int(HexToByte( ((binascii.hexlify(ser.read(1))).hex()) ),16)
-             if status == "1"
-               pwr-flag = "OFF"
-             if status == "0"
-               pwr-flag = "ERROR"
-             return
-          else:
+         if status != b'':
+           status =  HexToByte( ((binascii.hexlify(status)).hex()) )
+           if status == "FFA7":
+             sampled = "True"
+             status = int(HexToByte( ((binascii.hexlify(ser.read(1))).hex()) ),16)
+           if status == "1":
+             pwr_status = "OFF"
+           if status == "0":
+             pwr_status = "ERROR"
+             return (pwr_status)
+         else:
             continue
 
 def ReadSample():
     """
     Read exactly one sample from the default mode streaming samples
     """
-ser.flushInput() #flush input buffer
-
-sampled = False
-
-while not sampled:
-  sample = ser.read(2)
-#blank line check filter
-  if sample != b'':
-    reading = HexToByte( ((binascii.hexlify(sample)).hex()) )
-    if reading == "424d":
-       sampled = True
-       status = ser.read(8)
-       PM1 = int(HexToByte( ((binascii.hexlify(ser.read(2))).hex()) ),16)
-       PM25 = int(HexToByte( ((binascii.hexlify(ser.read(2))).hex()) ),16)
-       PM10 = int(HexToByte( ((binascii.hexlify(ser.read(2))).hex()) ),16)
-       print ("PM1 level: ", PM1, end="")
-       print (" PM2.5 level: ", PM25, end="")
-       print (" PM10 level: ", PM10 )
-       return ( PM1, PM2.5, PM10 )
-    else:
-      continue
+    ser.flushInput() #flush input buffer
+    sampled = False
+    while not sampled:
+      sample = ser.read(2)
+    #blank line check filter
+      if sample != b'':
+        reading = HexToByte( ((binascii.hexlify(sample)).hex()) )
+        if reading == "424d":
+          sampled = True
+          status = ser.read(8)
+          PM1 = int(HexToByte( ((binascii.hexlify(ser.read(2))).hex()) ),16)
+          PM25 = int(HexToByte( ((binascii.hexlify(ser.read(2))).hex()) ),16)
+          PM10 = int(HexToByte( ((binascii.hexlify(ser.read(2))).hex()) ),16)
+          return  PM1, PM25, PM10
+      else:
+        continue
 #
 #  End File
 #
